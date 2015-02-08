5b3e0afd759fc4b40e70b726a291b7a6a93f42a5
Juyeon Yun <yunjuyeon@gmail.com>
2015-02-08 15:33:37 -0600

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,9 @@
+# Plot 3
+plot(df_sub$Timestamp, df_sub$Sub_metering_1, col="black", type="l", ylab="Energy sub metering", xlab="")
+lines(df_sub$Timestamp, df_sub$Sub_metering_2, col="red")
+lines(df_sub$Timestamp, df_sub$Sub_metering_3, col="blue")
+legend("topright", 
+       col=c("black", "red", "blue"), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=c(1,1), lwd=c(1,1))
+dev.copy(png, file="plot3.png", width=480, height=480)
