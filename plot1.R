462e1390fd5313be3af153dbcca960fa1bd1bf29
Juyeon Yun <yunjuyeon@gmail.com>
2015-02-08 15:32:54 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+# Plot 1
+hist(df_sub$Global_active_power, 
+     col = "red",
+     xlab = "Global Active Power (kilowatts)",
+     main="Global Active Power")
+dev.copy(png, file="plot1.png", width=480, height=480)
