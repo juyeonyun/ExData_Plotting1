36fe73109e944c642cba5d2d9f58f662a067fcf3
Juyeon Yun <yunjuyeon@gmail.com>
2015-02-08 15:32:12 -0600

    Create loaddata.R

---
+++ loaddata.R
@@ -0,0 +1,12 @@
+# Load dataset into R
+
+filename <- "household_power_consumption.txt"
+
+df <- read.table(filename, header=TRUE, sep=";", na.strings="?")
+
+# subset the data only from 2/1/2007 and 2/2/2007
+df_sub <- df[df$Date %in% c("1/2/2007", "2/2/2007"),]
+
+# convert date and time variables to Date/Time class
+df_sub$Timestamp <- strptime(paste(df_sub$Date, df_sub$Time), "%d/%m/%Y %H:%M:%S")
+df_sub$Date <- as.Date(df_sub$Date, "%d/%m/%Y")
